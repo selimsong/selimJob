d795dd183efd9842208f34d8d84cb6d24cf7c482
selim song <selim@e.com>
2013-12-24 17:03:13 +0800

    'm'

--- weixin.php
+++ weixin.php
@@ -1,2 +1,14 @@
 <?php
-include_once("validate.php");
+//include_once("validate.php");
+
+$post_data = $GLOBALS["HTTP_RAW_POST_DATA"];
+$xml       = simplexml_load_string($post_data);
+
+$toUserName = $xml->ToUserName;
+$FromUserName = $xml->FromUserName;
+$MsgTyp       = $xml->MsgTyp;
+$Content      = $xml->Content;
+if(1){
+file_put_contents("wei_post.txt",  $post_data, FILE_APPEND);
+file_put_contents("wei_test.txt",  $toUserName.'-'.$FromUserName.$MsgTyp.$Content, FILE_APPEND);
+}
