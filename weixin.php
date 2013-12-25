c3be97ee52cf32d531292651bb9ca775cf35b618
selim song <selim@e.com>
2013-12-25 11:04:03 +0800

    'm'

--- weixin.php
+++ weixin.php
@@ -22,7 +22,7 @@ if ($msgType == 'event') {
 }
 
 if ($msgType == 'text') {	
-	replyTextAndImg($sendUserId, $developerId, '标题', '', '', '');
+	replyTextAndImg($sendUserId, $developerId, '制作你的#2014,心愿潮动#贺卡', '', '', '');
 	// replyText($sendUserId, $developerId, '发送“贺卡”，参加#2014，心愿潮动#贺卡，活动。');
 	 exit();
 }
@@ -68,13 +68,13 @@ function replyTextAndImg($toUserName, $fromUserName, $title, $description, $picU
 					<ArticleCount>%s</ArticleCount>
                     <Articles>
 					<item>
-					
+					<Title><![CDATA[%s]]></Title> 
 					<Description><![CDATA[%s]]></Description>
 					<PicUrl><![CDATA[%s]]></PicUrl>
 					<Url><![CDATA[%s]]></Url>
 					</item>
 					</Articles>
                </xml>";			
-	$resultStr = sprintf($textTpl, $toUserName, $fromUserName, time(), $count,  $description, $picUrl, $url);
+	$resultStr = sprintf($textTpl, $toUserName, $fromUserName, time(), $count, $title, $description, $picUrl, $url);
 	echo $resultStr;
 }
