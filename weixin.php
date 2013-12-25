03bb03557b2f6013925eb3ed1b22e3c44f9c058b
selim song <selim@e.com>
2013-12-25 08:42:14 +0800

    'm'

--- weixin.php
+++ weixin.php
@@ -9,7 +9,33 @@ $FromUserName = $xml->FromUserName;
 $MsgType       = $xml->MsgType;
 $Content      = $xml->Content;
 $MsgId        = $xml->MsgId;
+//subscribe event
+if ($msgType == 'event') {
+	if ($postObj->Event == 'subscribe') {
+		replyText($toUserName, $FromUserName, '欢迎加入统一冰红茶，赶快上传一张你跑步的照片（竖版）就可以制作一张属于你自己的#祝福贺卡#海报，快来试试！');
+	}
+	exit();
+}
+
+
+
 if(1){
 file_put_contents("wei_post.txt",  $post_data, FILE_APPEND);
 file_put_contents("wei_test.txt",  $toUserName.'-'.$FromUserName.'-'.$MsgType.'-'.$Content.'-'.$MsgId, FILE_APPEND);
+}
+
+
+
+function replyText($wxSignal, $username, $text)
+{
+	$textTpl = "<xml>
+				<ToUserName><![CDATA[%s]]></ToUserName>
+				<FromUserName><![CDATA[%s]]></FromUserName>
+				<CreateTime>%s</CreateTime>
+				<MsgType><![CDATA[text]]></MsgType>
+				<Content><![CDATA[%s]]></Content>
+				</xml>";
+				
+	$resultStr = sprintf($textTpl, $username, $wxSignal, time(), $text);
+	echo $resultStr;
 }
