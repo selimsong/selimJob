f63b6b21769430f25e48d236c424e7b7611f37b9
selim song <selim@e.com>
2013-12-25 09:39:53 +0800

    'm'

--- weixin.php
+++ weixin.php
@@ -4,15 +4,15 @@
 $post_data = $GLOBALS["HTTP_RAW_POST_DATA"];
 $xml       = simplexml_load_string($post_data);
 
-$toUserName = $xml->ToUserName;
-$fromUserName = $xml->FromUserName;
-$msgType       = $xml->MsgType;
+$developerId  = $xml->ToUserName;
+$sendUserId   = $xml->FromUserName;
+$msgType      = $xml->MsgType;
 $content      = $xml->Content;
 $msgId        = $xml->MsgId;
 //subscribe event
 if ($msgType == 'event') {
 	if ($xml->Event == 'subscribe') {
-		replyText($fromUserName, $toUserName, '欢迎加入统一冰红茶，赶快上传一张你跑步的照片（竖版）就可以制作一张属于你自己的#祝福贺卡#海报，快来试试！');
+		replyText($sendUserId, $developerId, '欢迎加入统一冰红茶，赶快上传一张你跑步的照片（竖版）就可以制作一张属于你自己的#祝福贺卡#海报，快来试试！');
 		file_put_contents("wei_subscribe.txt",  $post_data, FILE_APPEND);
 	}
 	exit();
@@ -20,7 +20,7 @@ if ($msgType == 'event') {
 
 if ($msgType == 'text') {
 	
-	 replyText($fromUserName, $toUserName, 'welcome to our wold');
+	 replyText($sendUserId, $developerId, '欢迎加入统一冰红茶, 上传照片吧');
     
 	exit();
 }
@@ -35,7 +35,6 @@ file_put_contents("wei_test.txt",  $toUserName.'-'.$FromUserName.'-'.$MsgType.'-
 
 function replyText($toUserName, $fromUserName, $text)
 {
-    
     $textTpl = "<xml>
 	                <ToUserName><![CDATA[%s]]></ToUserName>
 	                <FromUserName><![CDATA[%s]]></FromUserName>
@@ -44,7 +43,6 @@ function replyText($toUserName, $fromUserName, $text)
                     <Content><![CDATA[%s]]></Content>
                </xml>";			
 	$resultStr = sprintf($textTpl, $toUserName, $fromUserName, time(), $text);
-	file_put_contents("wei_text.txt",  $resultStr, FILE_APPEND);
 	echo $resultStr;
 }
 
