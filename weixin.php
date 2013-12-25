1932873fd09f9aeca36bc817f6e129cf9a07e898
selim song <selim@e.com>
2013-12-25 11:32:08 +0800

    'm'

--- weixin.php
+++ weixin.php
@@ -18,15 +18,14 @@ if ($msgType == 'event') {
 }
 
 if ($msgType == 'text') {
-
-	 replyText($sendUserId, $developerId, '发送“贺卡”，参加#2014，心愿潮动#贺卡，活动。');
+     $description = "制作你的#2014,心愿潮动#贺卡    仅需三步，轻松制作: 1.即刻在冰红茶对话框中上传一张你想发送的朋友的照片。 2.输入你对朋友在新的一年的祝愿。 3.获取专属于你的#新年新潮#贺卡。";
+	 replyTextAndImg($sendUserId, $developerId, '制作你的#2014,心愿潮动#贺卡', $description, 'http://115.29.49.54/intro.jpg', '',2);
+	 //replyText($sendUserId, $developerId, '发送“贺卡”，参加#2014，心愿潮动#贺卡，活动。');
 	 exit();
 }
 
 
 if ($msgType == 'image') {
-
-	 
 	 replyText($sendUserId, $developerId, '感谢您上传的照片');
      file_put_contents("wei_post.txt",  $post_data, FILE_APPEND);
 	 exit();
@@ -56,12 +55,13 @@ function replyText($toUserName, $fromUserName, $text)
 
 function replyTextAndImg($toUserName, $fromUserName, $title, $description, $picUrl='', $url='', $count=1)
 {
+  if(1 == $count){
     $textTpl = "<xml>
 	                <ToUserName><![CDATA[%s]]></ToUserName>
 	                <FromUserName><![CDATA[%s]]></FromUserName>
                     <CreateTime>%s</CreateTime>
                     <MsgType><![CDATA[news]]></MsgType>
-					<ArticleCount>%s</ArticleCount>
+					<ArticleCount>1</ArticleCount>
                     <Articles>
 					<item>
 					<Title><![CDATA[%s]]></Title> 
@@ -71,6 +71,34 @@ function replyTextAndImg($toUserName, $fromUserName, $title, $description, $picU
 					</item>
 					</Articles>
                </xml>";			
-	$resultStr = sprintf($textTpl, $toUserName, $fromUserName, time(), $count, $title, $description, $picUrl, $url);
+	$resultStr = sprintf($textTpl, $toUserName, $fromUserName, time(), $title, $description, $picUrl, $url);
+  }else{
+      $textTpl = "<xml>
+	                <ToUserName><![CDATA[%s]]></ToUserName>
+	                <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[news]]></MsgType>
+					<ArticleCount>2</ArticleCount>
+                    <Articles>
+					<item>
+					<Title><![CDATA[%s]]></Title> 
+					<Description><![CDATA[%s]]></Description>
+					<PicUrl><![CDATA[%s]]></PicUrl>
+					<Url><![CDATA[%s]]></Url>
+					</item>
+					<item>
+					<Title><![CDATA[%s]]></Title> 
+					<Description><![CDATA[%s]]></Description>
+					<PicUrl><![CDATA[%s]]></PicUrl>
+					<Url><![CDATA[%s]]></Url>
+					</item>
+					</Articles>
+               </xml>";			
+	$resultStr = sprintf($textTpl, $toUserName, $fromUserName, time(),  $title, $description, $picUrl, $url,$title, $description, '', '');
+  
+  
+  
+  
+  }
 	echo $resultStr;
 }
