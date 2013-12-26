613aa16d66341630b8945e4f223b20faacf376a1
selim song <selim@e.com>
2013-12-26 18:10:28 +0800

    'm'

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 $userImg = new Imagick('e-card.jpg');
 
@@ -17,7 +17,7 @@ $draw->setFontSize( 30 );
 $draw->setGravity(1);
 
 /* Create text */
-$image->annotateImage($draw, 100, 200, -10, utf8_decode('我们 the lazy dog1'));
+$image->annotateImage($draw, 100, 200, -10, '我们 the lazy dog2');
 
 $userImg->compositeImage($image, Imagick::COMPOSITE_DEFAULT, 0, 0);
 /* Give image a format */
