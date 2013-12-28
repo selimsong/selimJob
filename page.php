fb1cc5e59046ab6b8a9573615232450f2d123a89
selim song <selim@e.com>
2013-12-28 14:00:21 +0800

    'm'

---
+++ page.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>统一冰红茶</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/x-icon" href="http://dove.u.qiniudn.com/favicon.ico" />
+        <style type="text/css">
+            html, body { margin:0; padding:0; }
+            body {
+                background: #efefef;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+            }
+            #container { margin:0px auto; }
+            body { color: #FFEA00; font-size:16px; }
+            h1 { text-align:center; }
+            div.c { text-align:center; }
+            #btnswrap { width:100%; position:relative; }
+            #btn_sina { position:relative; z-index:9; display:inline-block; margin:22% 0 0 34%; width:10%; text-align:center; }
+            #btn_tencent { position:relative; z-index:9; display:inline-block; width:11.2%; text-align:center; }
+            #btn_sina img, #btn_tencent img { width:100%; }
+            img { border:none; }
+        </style>
+    </head>
+    <body>
+        <div id="container">
+            <img style="width:100%;" src="header2.png" />
+            <div style="text-align:center; margin:0 auto;"><img style="width:92%;" alt="" src="http://115.29.49.54/image/big<?php echo $id; ?>" /></div>
+                        
+          
+        </div>
+       
+    </body>
+</html>
--- weixin.php
+++ weixin.php
@@ -144,7 +144,7 @@ if ($msgType == 'text') {
 
 
             $description = "＃马上你就红＃三招炮制祝福海报。即刻上传你的照片，写下你对TA的祝福语，就可获取专属于你对TA的祝福海报";
-	        replyTextAndImg($sendUserId, $developerId, '炮制＃马上你就红＃祝福海报', $description, 'http://115.29.49.54/image/'.$userInfo['picName'], 'http://tongyi.mei94.com/page.html');
+	        replyTextAndImg($sendUserId, $developerId, '炮制＃马上你就红＃祝福海报', $description, 'http://115.29.49.54/image/'.$userInfo['picName'], 'http://tongyi.mei94.com/page?id='.$userInfo['picName']);
 
             $UserBigPicture = './img/big'.$userInfo['picName'];
 			$userImgBig = new Imagick($UserBigPicture);
