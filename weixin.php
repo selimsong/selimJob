1e6eb04833599d94d41af04298ca8716f8d53958
selim song <selim@e.com>
2013-12-28 19:10:52 +0800

    'm'

--- weixin.php
+++ weixin.php
@@ -161,7 +161,7 @@ if ($msgType == 'text') {
 			$draw->setTextEncoding('utf-8');
 			$draw->setFontSize(36);
 			$draw->setGravity(1);
-			$imageBig->annotateImage($draw, 400, 764, -9, $userInfo['content']);
+			$imageBig->annotateImage($draw, 400, 761, -9, $userInfo['content']);
 			header('Content-type: image/jpg');
             $imageBig->writeImage('./image/big'.$userInfo['picName']);
 
