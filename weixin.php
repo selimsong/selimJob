5ca90f18c8b087b6f9c092ad52023f7db5f0380e
selim song <selim@e.com>
2013-12-28 15:04:24 +0800

    'm'

--- weixin.php
+++ weixin.php
@@ -151,7 +151,7 @@ if ($msgType == 'text') {
 			$imageBig = new Imagick('bigbuttom.png');
 			$image_topBig = new Imagick('fbigtop.png');
 			$userImgBig->rotateImage(new ImagickPixel('transparent'), -12.00); 
-			$imageBig->compositeImage($userImgBig, Imagick::COMPOSITE_DEFAULT, 190, 180);
+			$imageBig->compositeImage($userImgBig, Imagick::COMPOSITE_DEFAULT, 180, 190);
 			$imageBig->compositeImage($image_topBig, Imagick::COMPOSITE_DEFAULT, 0, 0);
 			$draw = new ImagickDraw();
 			$draw->setFillColor('#f8ec00');
