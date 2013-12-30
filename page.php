c53a67f298030ee57ea100b0f5516a8102add4c4
selim song <selim@e.com>
2013-12-30 09:32:25 +0800

    'm'

--- page.php
+++ page.php
@@ -1,53 +1,42 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8" />
-        <title>统一冰红茶</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" type="image/x-icon" href="http://dove.u.qiniudn.com/favicon.ico" />
-        <style type="text/css">
-            html, body { margin:0; padding:0; }
-            body {
-                background: #efefef;
-                -webkit-background-size: cover;
-                -moz-background-size: cover;
-                -o-background-size: cover;
-                background-size: cover;
-            }
-            #container { margin:0px auto; }
-            body { color: #541e14; font-size:14px; }
-            h1 { text-align:center; }
-            div.c { text-align:center; }
-            #btnswrap { width:100%; position:relative; }
-            #btn_sina { position:relative; z-index:9; display:inline-block; margin:22% 0 0 34%; width:10%; text-align:center; }
-            #btn_tencent { position:relative; z-index:9; display:inline-block; width:11.2%; text-align:center; }
-            #btn_sina img, #btn_tencent img { width:100%; }
-            img { border:none; }
-        </style>
-    </head>
-    <body>
-        <div id="container">
-            <img style="width:100%;" src="http://115.29.49.54/header2.png" />
-            <div style="text-align:center; margin:0 auto;">
-			<?php 
-            $path = 'http://115.29.49.54/heka.jpg';
-			$path = 'heka.jpg';
-			if(!empty($_GET['id'])){
-			  $path = 'http://115.29.49.54/image/big'.base64_decode($_GET['id']);
-			} 
-			?>
-			<img style="width:92%;" alt="" src="<?php echo $path; ?>" />
-			</div>
-            <div style="width:92%;text-align:left; margin:0 auto;
-            height:163px; ">
-            <p>收到小伙伴们为你亲手炮制的贺卡是不是激动万分？</p>
-            <p>赶紧长按图片，存到手机相册，留存这份难得的情谊。或分享到微信朋友圈，秀出你的惊喜！</p>
+<head>
+<meta charset="UTF-8" />
+<title>统一冰红茶</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style type="text/css">
+html, body { margin:0; padding:0; }
+body{
+color: #541e14; 
+font-size:14px;
+background: #efefef;
+-webkit-background-size: cover;
+-moz-background-size: cover;
+-o-background-size: cover;
+background-size: cover;}
+#container { margin:0px auto; }
+h1 { text-align:center; }
+img { border:none; }
+</style>
+</head>
+<body>
+<div id="container">
+<img style="width:100%;" src="http://115.29.49.54/header2.png" />
+<div style="text-align:center; margin:0 auto;">
+<?php 
+$path = 'http://115.29.49.54/heka.jpg';
+$path = 'heka.jpg';
+if(!empty($_GET['id'])){
+$path = 'http://115.29.49.54/image/big'.base64_decode($_GET['id']);
+} 
+?>
+<img style="width:92%;" alt="" src="<?php echo $path; ?>" />
+</div>
+<div style="width:92%;text-align:left; margin:0 auto;height:163px; ">
+<p>收到小伙伴们为你亲手炮制的贺卡是不是激动万分？</p>
+<p>赶紧长按图片，存到手机相册，留存这份难得的情谊。或分享到微信朋友圈，秀出你的惊喜！</p>
 <p>快关注统一冰红茶官方微信，你也可以亲手炮制新年贺卡。</p>
-         
-            </div>
-        
-     
-        </div>
-       
-    </body>
+</div>
+</div>
+</body>
 </html>
