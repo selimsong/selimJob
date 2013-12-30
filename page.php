337559b5feec21113807a9f16d65c3023a66105d
selim song <selim@e.com>
2013-12-30 16:46:53 +0800

    'm'

--- page.php
+++ page.php
@@ -39,12 +39,12 @@ $path = 'http://115.29.49.54/image/big'.base64_decode($_GET['id']);
 <div style="width:92%;text-align:left; margin:0 auto;padding:0px 8px 0px 8px;height:163px; ">
 <?php if(empty($_GET['id'])){ ?>
 <p class="title">三招定制新年贺卡</p>
-<p>1: 即刻在冰红茶对话框中上传一张祝福对象的竖版照片。</p>
+<p>1: 即刻在冰红茶对话框中上传一张祝福对象的照片（竖版或正方形）。</p>
 <p>2: 写下你对TA的祝福语。</p>
 <p>3:  获取你为TA私人定制的新年贺卡。</p>
 <br />
 <p class="title">发送或分享祝福</p>
-<p>@你要祝福的人，或分享至朋友圈、微博等平台，为TA们私人定制祝福，祝TA们马上走红运！</p>
+<p>@你要祝福的人，或分享至微信朋友圈、新浪微博、腾讯微博等平台，为TA们私人定制祝福，祝TA们马上走红运！</p>
 <p>赶紧定制一张新年贺卡吧！</p> <br /><br />
 <?php }else{ ?>
 <p>收到小伙伴们为你亲手炮制的贺卡是不是激动万分？</p>
