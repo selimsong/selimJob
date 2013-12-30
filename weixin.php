94461c4f1b310288aa85ab84eb6471cf37f413e4
selim song <selim@e.com>
2013-12-30 17:17:54 +0800

    'm'

--- weixin.php
+++ weixin.php
@@ -124,8 +124,8 @@ if ($msgType == 'text') {
 			$imageBig = new Imagick('newbigbuttom.png');
             if(($geo['height'] - $geo['width']) > 40){
 			   $image_topBig = new Imagick('chang-top.png');
-			   $text_x = 200;
-			   $text_y = 805;
+			   $text_x = 100;
+			   $text_y = 845;
 			}else{
 			   $image_topBig = new Imagick('zhen-top.png');
 			    $text_x = 400;
