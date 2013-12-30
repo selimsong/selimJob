64a3b5e1d0469176a2d9c21cb23a62eb8d9658e8
selim song <selim@e.com>
2013-12-30 14:36:53 +0800

    'm'

--- weixin.php
+++ weixin.php
@@ -38,6 +38,10 @@ if ($msgType == 'image') {
     list($width, $height) = getimagesize('./img/'.$picName);
 
 	$im_big = new Imagick('./img/'.$picName);
+	if(($width - $height - 30) > 0){
+		 replyText($sendUserId, $developerId, '不好意思，你的输入内容有误，如果想要参与统一冰红茶 #马上你就红#新年贺卡私人定制活动，请直接上传一张祝福对象的照片（要竖版或正方形的），就可以获取你为TA私人定制的新年贺卡，快来试试！');
+	     exit();
+	}
 	$h = NULL;
 	$_width = $width;
     $_height = $height;
