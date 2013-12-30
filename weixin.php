af06472bcde515bfbe2235a7fe674dadfdb98006
selim song <selim@e.com>
2013-12-30 13:30:59 +0800

    'm'

--- weixin.php
+++ weixin.php
@@ -82,7 +82,7 @@ if ($msgType == 'image') {
 	}
 
     replyText($sendUserId, $developerId, '新年快乐！欢迎参加＃马上你就红＃新年贺卡制作活动，你的照片已收到！现在，请在“祝你2014马上______”的空格处写下一段祝福语（请将文字限制在10个英文字符内，请勿夹杂符号表情或敏感文字），活动时间（2013/12/31—2014/2/6）');
-    $doc = array('developerId' => "$developerId", 'sendUserId' => "$sendUserId", 'picUrl'=> "$picUrl", 'picName' => "$picName", 'mediaId'=> "$mediaId", 'updateData' => date('d'), 'updatetime' => "$time");
+    $doc = array('developerId' => "$developerId", 'status'=> '1', 'sendUserId' => "$sendUserId", 'picUrl'=> "$picUrl", 'picName' => "$picName", 'mediaId'=> "$mediaId", 'updateData' => date('d'), 'updatetime' => "$time");
 	$collection_log->insert($doc); 
 	exit();
 }
@@ -130,6 +130,8 @@ if ($msgType == 'text') {
 			$imageBig->annotateImage($draw, 400, 761, -9, $userInfo['content']);
 			header('Content-type: image/jpg');
             $imageBig->writeImage('./image/big'.$userInfo['picName']);
+			$doc = array('developerId' => "$developerId", 'sendUserId' => "$sendUserId",  'status'=> '3', 'updateData' => date('d'), 'updatetime' => "$time");
+	        $collection_log->insert($doc); 
 		 }else{
             $badkey = '妈逼|操|狗屎|阴|垃圾';
 			if(preg_match("/$badkey/i", $content)){
@@ -143,7 +145,7 @@ if ($msgType == 'text') {
 		    $newContent  = array('$set' => array('content' => "$content", 'flg'=>'2'));
             $collection->update(array('sendUserId' => "$sendUserId", 'updateData' => date('d')),  $newContent);
 
-			$doc = array('developerId' => "$developerId", 'sendUserId' => "$sendUserId",  'content'=> "", 'updateData' => date('d'), 'updatetime' => "$time");
+			$doc = array('developerId' => "$developerId", 'sendUserId' => "$sendUserId",,  'status'=> '2', 'content'=> "$content", 'updateData' => date('d'), 'updatetime' => "$time");
 	        $collection_log->insert($doc); 
 		    replyText($sendUserId, $developerId, '你的照片和文字已上传成功，红运马上送到TA！请确认你已经认真阅读过我们的<a href="http://tongyi.mei94.com/rule.html">用户条款和隐私政策</a>，回复”Y”表示同意并继续。
 '); 
