c938eb84b213cbf04b8c72f259300728e1f0965f
selim song <selim@e.com>
2013-12-31 14:14:06 +0800

    'm'

--- weixin.php
+++ weixin.php
@@ -10,7 +10,7 @@ $msgId        = $xml->MsgId;
 if ($msgType == 'event') {
 	if ($xml->Event == 'subscribe') {
 			 $description = "新年到了，小伙伴们是不是按捺不住要给大伙儿发祝福呢？最酷莫过于亲手定制一张贺卡，即刻在对话框中上传一张祝福对象的照片（竖版或正方形），获取你为TA私人定制的新年贺卡。";
-	        replyTextAndImg($sendUserId, $developerId, '#心意自造#潮卡送到', $description, 'http://115.29.49.54/activity.jpg?v=123', 'http://tongyi.mei94.com/page');
+	        replyTextAndImg($sendUserId, $developerId, '#心意自造#潮卡送到', $description, 'http://115.29.49.54/activity.jpg?v=123', 'http://tongyi.mei94.com/page?v=1234');
 	}
 	exit();
 }
